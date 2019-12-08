4ab161cefcd5ed92cbdb8feee885018df86c575d
Andrew <ahinh93@gmail.com>
2019-12-08 14:12:35 -0600

    added private json key to gitignore

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,13 @@
 const functions = require('firebase-functions');
 
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://amberleaf-app.firebaseio.com'
+  });
 
+// Initialize the default app
+var admin = require('firebase-admin');
+var app = admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
