ee1ffec36bb1f058484b3e2542cba97b939ebd12
Andrew <ahinh93@gmail.com>
2019-12-08 20:22:44 -0600

    switch from firestore to realtime database

--- public/amberleaf-scripts.js
+++ public/amberleaf-scripts.js
@@ -2,62 +2,47 @@
  * Written by Andrew Hinh
 */
 
-// Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyD47NOjIbQYeNZtwbIEqTNfRZSDmAp1E14",
-    authDomain: "amberleaf-app.firebaseapp.com",
-    databaseURL: "https://amberleaf-app.firebaseio.com",
-    projectId: "amberleaf-app",
-    storageBucket: "amberleaf-app.appspot.com",
-    messagingSenderId: "522203017902",
-    appId: "1:522203017902:web:1b9fab7ae49d0670d0b33f",
-    measurementId: "G-HYLXXYF545"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-function saveData() {
-    var fname = document.getElementById('first-name').value;
-    var lname = document.getElementById('last-name').value;
-    writeUserData(fname, lname);
-}
-function writeUserData(fname, lname) {
-    alert("write to 1");
-    //create reference to database service
-    alert(typeof firebase);
-    var database = firebase.database().ref("projectData/server");
-    alert("write to 2");
-
-    var usersRef = database.child("server");
-
-    alert("write to 3");
-    usersRef.set({
-        alanisawesome: {
-            date_of_birth: "June 23, 1912",
-            full_name: "Alan Turing"
-        },
-        gracehop: {
-            date_of_birth: "December 9, 1906",
-            full_name: "Grace Hopper"
-        }
-    });
-    alert("write to 4");
-    // alert("done");
-}
-function retrieveDatabase() {
-    alert("retrieve 1")
-    var db = admin.database();
-    alert("retrieve 2");
-    var ref = db.ref("projectData/server");
-    alert("retrieve 3");
-    ref.on("value",function(snapshot){
-        document.getElementById("dataText").innerHTML = "yes";
-    }, function (errorObject) {
-        document.getElementById("dataText").innerHTML = "error";
-    });
-}
+// function saveData() {
+//     var
+//     var fname = document.getElementById('first-name').value;
+//     var lname = document.getElementById('last-name').value;
+//     alert("write to 1");
+//     let docRef = db.collection('users').doc('alovelace');
+//     alert("write to 2");
+//     let setAda = docRef.set({
+//         first: 'Ada',
+//         last: 'Lovelace',
+//         born: 1815
+//     });
+//     alert("write to 3");
+// }
+// function retrieveDatabase() {
+//     alert("retrieve 1")
+//     var db = admin.database();
+//     alert("retrieve 2");
+//     var ref = db.ref("projectData/server");
+//     alert("retrieve 3");
+//     ref.on("value", function (snapshot) {
+//         document.getElementById("dataText").innerHTML = "yes";
+//     }, function (errorObject) {
+//         document.getElementById("dataText").innerHTML = "error";
+//     });
+// }
 
 function clearTextBox() {
     document.getElementById("dataText").innerHTML = "";
+}
+
+function testVariable() {
+    alert("hi");
+    document.location.href = "./testVariable.html";
+}
+
+function writeToDatabase(){
+    if (typeof firebase !== 'undefined') {
+        // the variable is defined
+        alert("firebase defined");
+    }
+    else
+        alert("firebase undefined");
 }
