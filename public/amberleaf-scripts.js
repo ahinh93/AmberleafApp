e155aa3f61806c1076c83c621fd9a65ac95153cc
Andrew <ahinh93@gmail.com>
2019-12-11 14:21:33 -0600

    successfully created read/write to DB

--- public/amberleaf-scripts.js
+++ public/amberleaf-scripts.js
@@ -2,47 +2,21 @@
  * Written by Andrew Hinh
 */
 
-// function saveData() {
-//     var
-//     var fname = document.getElementById('first-name').value;
-//     var lname = document.getElementById('last-name').value;
-//     alert("write to 1");
-//     let docRef = db.collection('users').doc('alovelace');
-//     alert("write to 2");
-//     let setAda = docRef.set({
-//         first: 'Ada',
-//         last: 'Lovelace',
-//         born: 1815
-//     });
-//     alert("write to 3");
-// }
-// function retrieveDatabase() {
-//     alert("retrieve 1")
-//     var db = admin.database();
-//     alert("retrieve 2");
-//     var ref = db.ref("projectData/server");
-//     alert("retrieve 3");
-//     ref.on("value", function (snapshot) {
-//         document.getElementById("dataText").innerHTML = "yes";
-//     }, function (errorObject) {
-//         document.getElementById("dataText").innerHTML = "error";
-//     });
-// }
+function writeToDatabase() {
 
-function clearTextBox() {
-    document.getElementById("dataText").innerHTML = "";
-}
+    if (typeof database !== 'undefined') {
+        database.ref('object/').set(document.getElementById('myInput'));
 
-function testVariable() {
-    alert("hi");
-    document.location.href = "./testVariable.html";
-}
-
-function writeToDatabase(){
-    if (typeof firebase !== 'undefined') {
-        // the variable is defined
-        alert("firebase defined");
     }
     else
-        alert("firebase undefined");
+        console.log("database undefined");
+}
+function listenToDatabase() {
+    const preObject = document.getElementById('object');
+
+    //create references
+    const dbRefObject = firebase.database().ref().child('object');
+
+    //sync object changes
+    dbRefObject.on('value', snap => console.log(snap.val()));
 }
--- public/init-firebase.js
+++ public/init-firebase.js
@@ -1,12 +1,39 @@
+
 // Set the configuration for your app
 // TODO: Replace with your project's config object
-var config = {
-    apiKey: "apiKey",
-    authDomain: "projectId.firebaseapp.com",
-    databaseURL: "https://databaseName.firebaseio.com",
-    storageBucket: "bucket.appspot.com"
+var firebaseConfig = {
+  apiKey: "AIzaSyD47NOjIbQYeNZtwbIEqTNfRZSDmAp1E14",
+  authDomain: "amberleaf-app.firebaseapp.com",
+  databaseURL: "https://amberleaf-app.firebaseio.com",
+  projectId: "amberleaf-app",
+  storageBucket: "amberleaf-app.appspot.com",
+  messagingSenderId: "522203017902",
+  appId: "1:522203017902:web:1b9fab7ae49d0670d0b33f",
+  measurementId: "G-HYLXXYF545"
 };
-firebase.initializeApp(config);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // Get a reference to the database service
 var database = firebase.database();
+
+
+function writeToDatabase() {
+
+  if (typeof database !== 'undefined') {
+    console.log("input:" + document.getElementById('myInput').value);
+    database.ref('object/').set(document.getElementById('myInput').value);
+
+  }
+  else
+    console.log("database undefined");
+}
+function listenToDatabase() {
+  const preObject = document.getElementById('object');
+
+  //create references
+  const dbRefObject = firebase.database().ref().child('object');
+
+  //sync object changes
+  dbRefObject.on('value', snap => console.log(snap.val()));
+}
