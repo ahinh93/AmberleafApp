99daa7c7c860ae35d3d5b780f2304373bf99ac9f
Andrew <ahinh93@gmail.com>
2019-12-13 16:20:56 -0600

    fixed bug to side panel

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,11 @@
-var admin = require('firebase-admin');
-var functions = require('firebase-functions');
+// var admin = require('firebase-admin');
+// var functions = require('firebase-functions');
 
-// Initialize the default app
-var defaultApp = admin.initializeApp(functions.config().firebase);
+// // Initialize the default app
+// var defaultApp = admin.initializeApp(functions.config().firebase);
 
-console.log(defaultApp.name);  // '[DEFAULT]'
+// // console.log(defaultApp.name);  // '[DEFAULT]'
 
-// Retrieve services via the defaultApp variable...
-var defaultAuth = defaultApp.auth();
-var db = admin.firestore();
+// // Retrieve services via the defaultApp variable...
+// var defaultAuth = defaultApp.auth();
+// var db = admin.firestore();
